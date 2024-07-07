a01581f012e8b84fbdff1231fef60cda58697985
mytest31 <mytest.zoomcamp1@gmail.com>
2024-07-07 17:11:21 +0300

    feat: integrate code quality tools

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,18 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh',  'react-compiler'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    "react-compiler/react-compiler": "error",
+    'prettier/prettier': 'error',
   },
 }
